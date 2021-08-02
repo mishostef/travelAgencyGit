977c1a1c70824eed2b3083e931b1241031bc2c78
mishostef <mishostef@abv.bg>
2021-08-02 23:51:59 +0300

    added new components

--- client/travel-agency/src/app/trip/trip.module.ts
+++ client/travel-agency/src/app/trip/trip.module.ts
@@ -5,14 +5,18 @@ import { TripListComponent } from './trip-list/trip-list.component';
 import { TripService } from './trip.service';
 import { TripDetailsComponent } from './trip-details/trip-details.component';
 import { RouterModule } from '@angular/router';
+import { TripListPromoComponent } from './trip-list-promo/trip-list-promo.component';
+import { TripListVacationsComponent } from './trip-list-vacations/trip-list-vacations.component';
+import { TripListNewComponent } from './trip-list-new/trip-list-new.component';
+import { TripListExcursionsComponent } from './trip-list-excursions/trip-list-excursions.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports:[TripComponent, TripListComponent,TripDetailsComponent],
+  exports:[TripComponent, TripListComponent, TripDetailsComponent, TripListPromoComponent, TripListVacationsComponent, TripListNewComponent, TripListExcursionsComponent],
   providers:[TripService],
-  declarations: [TripComponent, TripListComponent, TripDetailsComponent]
+  declarations: [TripComponent, TripListComponent, TripDetailsComponent, TripListPromoComponent, TripListVacationsComponent, TripListNewComponent, TripListExcursionsComponent]
 })
 export class TripModule { }
