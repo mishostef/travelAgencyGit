4ed5ac2f559baef2fb341e1ec7605d287026c42e
mishostef <mishostef@abv.bg>
2021-08-02 23:54:17 +0300

    unsubscribe added

--- client/travel-agency/src/app/trip/trip-list-promo/trip-list-promo.component.ts
+++ client/travel-agency/src/app/trip/trip-list-promo/trip-list-promo.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ITrip } from 'app/shared/interfaces/trip';
+import { Subscription } from 'rxjs';
 import { TripService } from '../trip.service';
 @Component({
   selector: 'app-trip-list-promo',
   templateUrl: './trip-list-promo.component.html',
   styleUrls: ['./trip-list-promo.component.scss']
 })
-export class TripListPromoComponent implements OnInit {
+export class TripListPromoComponent implements OnInit,OnDestroy {
   promoted: ITrip[] = [];
+  subscription: Subscription;
 
   constructor(private travelService: TripService) { }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   ngOnInit() {
-    this.travelService.getPromoted().
+   this.subscription = this.travelService.getPromoted().
       subscribe(res => {
         this.promoted = (JSON.parse(res['_body']) as ITrip[])
           .map(x => {
@@ -20,7 +25,6 @@ export class TripListPromoComponent implements OnInit {
             return x;
           });
       });
-
   }
 
 }
