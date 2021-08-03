27df77452ad86a836307e50727ec131e7b62ecc6
mishostef <mishostef@abv.bg>
2021-08-03 23:39:32 +0300

    added getTripFrom Response

--- client/travel-agency/src/app/shared/utils.ts
+++ client/travel-agency/src/app/shared/utils.ts
@@ -1,3 +1,5 @@
+import{ITrip} from './interfaces/trip';
+
 export function getUserId(): string {
     const token = localStorage.getItem('authToken');
     if (!token) throw new Error(`You are not signed in!`)
@@ -24,8 +26,18 @@ export function getPropFromResponse(res, name) {
 export function splitIntoThree(all) {
     const length = all.length;
     const n = Math.ceil(length / 3);
-    const left = all.slice(0, n);    
-    const center = (length % 3 == 2)||(length%3==0) ? all.slice(n, 2 * n) : all.slice(n, 2 * n - 1);
+    const left = all.slice(0, n);
+    const center = (length % 3 == 2) || (length % 3 == 0) ? all.slice(n, 2 * n) : all.slice(n, 2 * n - 1);
     const right = all.slice(n + center.length);
     return { left, center, right };
+}
+
+export function getTripsFromResponse(res) {
+    const excursions = (JSON.parse(res['_body']) as ITrip[])
+        .map(x => {
+            x.img = x.img.includes(`https`) ? x.img : `../../../assets/${x.img}.jpg`;
+            return x;
+        });
+
+    return excursions;
 }
