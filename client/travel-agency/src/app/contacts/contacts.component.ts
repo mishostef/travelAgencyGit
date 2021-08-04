74383356d9873aa16a99484ef457c5333b68e02e
mishostef <mishostef@abv.bg>
2021-08-04 17:44:07 +0300

    added encodeUri

--- client/travel-agency/src/app/contacts/contacts.component.ts
+++ client/travel-agency/src/app/contacts/contacts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
+import { UrlSerializer } from '@angular/router';
 
 @Component({
   selector: 'app-contacts',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
   address: string = "8, Bacho Kiro street";
+  addressCyrillic = "Бачо Киро 8";
+  url = 'https://maps.google.com/maps?q=sofia' + encodeURI(this.addressCyrillic) +
+    "&t=&z=13&ie=UTF8&iwloc=&output=embed"
+
   tel: string = "02777777";
-  manager:string= "Man Ager";
-  constructor() { }
+  manager: string = "Man Ager";
+
+  trustedUrl: string = '';
+  constructor(private sanitizer: DomSanitizer) {
+    this.trustedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url) as string;
+  }
+
 
   ngOnInit() {
   }
