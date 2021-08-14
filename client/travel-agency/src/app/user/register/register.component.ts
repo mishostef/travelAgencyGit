d143425c76b785cb55dbff2b3d3bbd63bfd1d4cd
mishostef <mishostef@abv.bg>
2021-08-14 11:20:14 +0300

    added deactivate guard

--- client/travel-agency/src/app/user/register/register.component.ts
+++ client/travel-agency/src/app/user/register/register.component.ts
@@ -1,28 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+  formchanges = false;
+  subscription: Subscription =new Subscription();
+
+  @ViewChild('registerForm') ngForm: NgForm;
 
   constructor(private userService: UserService,
     private router: Router) { }
 
-  ngOnInit() {
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
+
+  ngOnInit() {
+    const sub1 = this.ngForm.form.valueChanges.subscribe(x => {
+      console.log(x);
+      this.formchanges = true;
+    })
+    this.subscription.add(sub1);
+  }
+
+  hasChanges(){
+    return this.formchanges;
+  }
+
   handleSubmit({ email, passwords: { password } }: { email: string, passwords: { password: string } }) {
-    this.userService.registerUser(email, password).subscribe((res) => {
+    const sub2 = this.userService.registerUser(email, password).subscribe((res) => {
 
       const x = res['_body'];
       const token = JSON.parse(x)['accessToken'];
       document.cookie = JSON.parse(x)['accessToken']
       localStorage.setItem('authToken', token);
-      this.router.navigate([''])   
-      
+      this.router.navigate([''])
+
     }, console.error);
+    this.subscription.add(sub2);
   }
+
 }
