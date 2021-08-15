5c002c586cee959c7fae35051baa5332f2c2bf0a
mishostef <mishostef@abv.bg>
2021-08-15 03:08:56 +0300

    small changes

--- client/travel-agency/src/app/user/user.module.ts
+++ client/travel-agency/src/app/user/user.module.ts
@@ -5,7 +5,7 @@ import { RegisterComponent } from './register/register.component';
 import { FormsModule } from '@angular/forms';
 import { ProfileComponent } from './profile/profile.component';
 import { TripModule } from 'app/trip/trip.module';
-import { ChartsModule } from 'angular-bootstrap-md';
+import { CarouselModule, ChartsModule } from 'angular-bootstrap-md';
 import { UserRoutingModule } from './user-routing.module';
 import { ConfirmDeactivateGuard } from 'app/shared/guards/confirm.deactivate';
 import { ChartComponent } from './chart/chart.component';
@@ -18,6 +18,7 @@ import { ChartComponent } from './chart/chart.component';
     FormsModule,
     TripModule,
     ChartsModule,
+    CarouselModule,
     UserRoutingModule
   ],
   exports: [LoginComponent, RegisterComponent],
