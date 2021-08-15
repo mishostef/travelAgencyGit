37d3d3de44d04d097eee0b9d16ac572cb7ce11b3
mishostef <mishostef@abv.bg>
2021-08-15 09:36:25 +0300

    added component

---
+++ client/travel-agency/src/app/user/chart/chart.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss']
+})
+export class ChartComponent implements OnInit {
+  @Input() chartData: Array<any> = [{}];
+  @Input() chartLabels: Array<any> = [];
+
+  public type: string = 'bar';
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: 'rgba(220,220,220,0.2)',
+      borderColor: 'rgba(220,220,220,1)',
+      borderWidth: 2,
+      pointBackgroundColor: 'rgba(220,220,220,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(220,220,220,1)'
+    },
+    {
+      backgroundColor: 'rgba(151,187,205,0.2)',
+      borderColor: 'rgba(151,187,205,1)',
+      borderWidth: 2,
+      pointBackgroundColor: 'rgba(151,187,205,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(151,187,205,1)'
+    }
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
